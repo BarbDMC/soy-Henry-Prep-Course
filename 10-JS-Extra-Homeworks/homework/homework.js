03bfb0db934693e7062246ce898dd40d3fe17d99
“BarbaraDMC” <barbaradmorantesc@gmail.com>
2022-08-14 14:47:39 -0500

    Completing homework 10-JS-Extra-Homeworks

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,14 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  const newArray = [];
+
+  for (const key in objeto) {
+    const objectToArray = [key, objeto[key]]
+    newArray.push(objectToArray);
+  }
+
+  return newArray;
 }
 
 
@@ -18,6 +26,20 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  const splitString = string.split('');
+  const letterObject = {};
+
+  splitString.forEach((letter) => {
+    if (letter in letterObject) {
+      letterObject[letter] = letterObject[letter] + 1
+    }
+
+    if (!(letter in letterObject)) {
+      letterObject[letter] = 1
+    }
+  });
+  
+  return letterObject;
 }
 
 
@@ -26,6 +48,15 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  const splitString = s.split('');
+  
+  const upperCaseLetters = splitString.filter((letter) => letter.toUpperCase() === letter);
+  const lowerCaseLetters = splitString.filter((letter) => letter.toLowerCase() === letter);
+  
+  const joinUpperLetters = upperCaseLetters.join('');
+  const joinLowerLetters = lowerCaseLetters.join('');
+
+  return `${joinUpperLetters}${joinLowerLetters}`;
 }
 
 
@@ -35,6 +66,19 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  const splitString = str.split(' ');
+
+  const reversePhrase = splitString.map((word) => {
+    const splitWord = word.split('');
+    const reverseWordArray = splitWord.reverse();
+    const joinReverseWord = reverseWordArray.join('');
+
+    return joinReverseWord;
+  });
+
+  const joinReversePhrase = reversePhrase.join(' ');
+
+  return joinReversePhrase;
 } 
 
 
@@ -43,6 +87,17 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  const originalNumber = numero.toString().split('')
+  const reverseNumber = numero.toString().split('').reverse();
+  let is_capicua = true;
+
+  for (let index = 0; index < originalNumber.length; index++) {
+    if (originalNumber[index] !== reverseNumber[index]) {
+      is_capicua = false;
+    }
+  }
+
+  return is_capicua ? 'Es capicua' : 'No es capicua';
 }
 
 
@@ -50,6 +105,9 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  const stringWithoutABC = cadena.replaceAll(/[abc]/g, '');
+
+  return stringWithoutABC;
 }
 
 
@@ -57,6 +115,9 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  const wordsByLength = arr.sort((previousWord, currentWord) => previousWord.length - currentWord.length);
+  
+  return wordsByLength;
 }
 
 
@@ -66,6 +127,11 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  const commonNumbers = arreglo1.filter((number) => {
+    return arreglo2.indexOf(number) !== -1;
+  });
+
+  return commonNumbers;
 }
 
 
